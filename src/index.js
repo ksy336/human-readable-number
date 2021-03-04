e536c4e8fba1fa540a1c2098c2e8dce1a59d7ca3
Kseniya Klimova <ksuoxy773@gmail.com>
2021-03-04 23:15:29 +0400

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+
 module.exports = function toReadable (number) {
-  
+
+    let smallNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let teenNum = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tyNum = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number === 0) {
+        return 'zero'
+    }
+    ;
+    if (number < 10) {
+        return smallNum[number];
+    } else if (number >= 10 && number < 20) {
+        return teenNum[number - 10];
+    } else if (number >= 20 && number < 100) {
+        if (number % 10 === 0) {
+            return `${tyNum[Math.floor(number / 10) - 2]}`;
+        } else {
+            return `${tyNum[Math.floor(number / 10) - 2]} ${smallNum[number % 10]}`;
+        }
+    } else if (number >= 100 && number < 1000) {
+        if (number % 100 === 0) {
+            return `${smallNum [Math.floor(number / 100)]} hundred`;
+        } else if (Math.floor(number / 10) % 10 === 0) {
+            return `${smallNum [Math.floor(number / 100)]} hundred ${smallNum [Math.floor(number % 10)]}`;
+        } else if (Math.floor(number / 10) % 10 === 1) {
+            return `${smallNum [Math.floor(number / 100)]} hundred ${teenNum[Math.floor(number % 10)]}`;
+        } else {
+            if (number % 10 === 0) {
+                return `${smallNum[Math.floor(number / 100)]} hundred ${tyNum[(Math.floor(number / 10) % 10) - 2]}`;
+            } else {
+                return `${smallNum[Math.floor(number / 100)]} hundred ${tyNum[(Math.floor(number / 10) % 10) - 2]} ${smallNum[Math.floor(number % 10)]}`;
+            }
+        }
+    }
 }
